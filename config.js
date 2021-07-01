9aae7a397dade023e826c91925cdf153e7c73b4d
Thuwhihi <86757988+Thuwhihi@users.noreply.github.com>
2021-07-01 10:52:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Xuân à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Lạc vào tim Thư lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    btnYes: 'Thích Thư chết mẹ còn hỏi<33',
+    btnNo: 'Không, Thư sao v.',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu Thư <3',
+    btnReply: 'Gửi cho Thư <3',
+    reply: 'Tai vi Thư xink đep tuyet voi đó hihi 🤣',
+    mess: 'Thư bít mà. Ngại quá hihi.',
+    messDesc: 'Cùng nhau hít dramma bạn yeu dấu.',
+    btnAccept: 'Okiiiii haha <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
